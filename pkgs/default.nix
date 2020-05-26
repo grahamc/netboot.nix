d9b4440b0de29f299d8de9ff31d2cf10406e371c
Graham Christensen <graham@grahamc.com>
2020-05-25 21:47:56 -0400

    note about what makeCpioRecursive is about.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,9 @@
 { callPackage }:
 {
+  # Create a cpio archive comprised of many initrd's appended to
+  # each other. Harder to debug, and requires recursive Nix support,
+  # but faster for iterating.
   makeCpioRecursive = callPackage ./cpio-recursive {};
+
   makeSquashfsManifest = callPackage ./squashfs-recursive {};
 }
